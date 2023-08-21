36d9e68e277e70a9b731f3b60fce10957cec7867
konain1 <kaunainkasab@gmail.com>
2023-08-21 23:47:38 +0530

    completed using width

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import ProgressBar from './component/ProgressBar';
+import {useState,useEffect,} from 'react'
+
 
 function App() {
+
+  const [value,setValue] = useState(0)
+
+  useEffect(() => {
+
+    setInterval(() => {
+      setValue((val)=> val + 1)
+    }, 100);
+    
+  }, [])
+
   return (
     <div className="App">
-    <ProgressBar />
+    <ProgressBar value={value}/>
      
     </div>
   );
--- src/component/ProgressBar.js
+++ src/component/ProgressBar.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {useState,useEffect,} from 'react'
+
 
 function ProgressBar({value = 0}) {
+
+    const [percent,setPercent] = useState(value)
+
+    useEffect(()=>{
+        // edge case max 100 min 0
+        setPercent(Math.min(100,Math.max(value,0)))
+
+    },[value])
+
   return (
     <div className='progressbar'>
-      <span> {value.toFixed()}%</span>
+
+      <span  style={{color:percent > 49 ? 'white' : 'black'}}> {percent.toFixed()}%</span>
+      <div style={{width:`${percent}%` } }></div>
+
     </div>
   )
 }
